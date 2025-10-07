89514905229057f6929cd04c8dd2d68300d06e02
marinagardella <marigardella@gmail.com>
2025-10-06 21:40:45 -0300

    fixed google api connection

--- app.py
+++ app.py
@@ -23,7 +23,10 @@ WORKSHEET_NAME = "resultados"
 def connect_to_gsheet():
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
     )
     client = gspread.authorize(credentials)
     sheet = client.open(SPREADSHEET_NAME).worksheet(WORKSHEET_NAME)
